37fbe5fc915f839f6a8f7a3ea758eb514f64a9bb
Luis Gustavo Pereira da Silva <luis800pereira@gmail.com>
2026-04-29 19:43:13 -0300

    exercicios repetição

--- ExerciciosRepetição.js
+++ ExerciciosRepetição.js
@@ -1,17 +1,17 @@
-//Contando de 1 a 10
+// 1 Contando de 1 a 10
 /*for (let i = 1; i <= 10; i++) {
     console.log(`Número: ${i}`)
 }*/
 
-//Tabuada de um Número
+// 2 Tabuada de um Número
 /*let numero = Number(prompt("Digite um Número:"));
 
-for (let i = 1; i <= 10; i++) {
-    const resultado = numero * i;
+for (let i2 = 1; i2 <= 10; i++) {
+    const resultado = numero * i2;
     console.log(`${numero} x ${i} = ${resultado}`);
 }*/
 
-//Soma de 1 até N
+// 3 Soma de 1 até N
 
 /*const N = Number(prompt("Digite um número:"));
 let soma = 0;
@@ -21,4 +21,25 @@ for (let i = 1; i <= N; i++) {
 
 console.log(`Soma de 1 até ${N} = ${soma}`);
 */
+
+// 1 Exibir os números pares de 1 a 50
+/*console.log("Números pares de 0 a 50\n");
+
+for (let i = 0; i <=50; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}*/
+
+
+// 3 COntagem regressiva
+let tempo = 15;
+console.log("Começando contagem regressiva...");
+
+while (tempo > 0) {
+    console.log(`${tempo}...`);
+
+    tempo--
+}
+console.log("Feliz Natal!!");
 
