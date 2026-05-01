8168589e7c338878865fbceaa067e3f365e027cf
Luis Gustavo Pereira da Silva <luis800pereira@gmail.com>
2026-04-30 21:56:08 -0300

    Aula Reduce

--- filter.js
+++ filter.js
@@ -41,9 +41,195 @@ alunos.forEach((nome, index) => {
 });
 console.log(maiores)
 */
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+/*const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
 const pares = numeros.filter((numero) => {
     return numero % 2 === 0;
 });
 
 console.log(pares)
+*/
+
+//Crie um array com vários números.
+//Use filter para mostrar apenas os números maiores que 50.
+
+/*const nums = [10,55,80,20,00];
+
+const maiores = nums.filter((n) => {
+    return n <50;
+})
+
+console.log(maiores);
+*/
+
+//Map
+/*const people = [
+    {name: "Lucas", age: 35},
+    {name: "Maria", age: 17},
+    {name: "João", age: 18},
+    {name: "Ana", age: 22},
+];*/
+
+/*const novoArray =  array.map((element) => {
+    return novoValor;
+});*/
+
+/*const nomes = people.map((person) => {
+    return person.name.toUpperCase();
+});
+console.log(nomes);
+*/
+
+/*const numeros = [1,2,3,4,5];
+
+const dobro = numeros.map((numero) => {
+    return numero * 2;
+})
+console.log(dobro);
+
+const produtos = ["Mouse", "Teclado", "Monitor"];
+
+const frases = produtos.map ((produto) => {
+    return `Produtos: ${produto}`
+});
+console.log(frases)
+*/
+
+/*const pessoas = [
+    {nome: "Lucas", idade: 35},
+    {nome: "Maria", idade: 17},
+];
+
+const resultado = pessoas.map((pessoa) => {
+    return {
+        nome: pessoa.nome,
+        maiorIdade: pessoa.idade >= 18
+    }
+});
+console.log(resultado);
+*/
+
+//Use map para transformar um array de preços em preços com desconto de 10%.
+
+/*const precos = [100, 200, 300];
+const numeros = precos.map ((preco) => {
+    return preco * 0.9;
+})
+console.log(numeros);
+*/
+
+//Use map para transformar: [1,2,3,4] em ["Número 1", "Número 2"...]
+
+/*const numeros = [1,2,3,4];
+const resultado = numeros.map ((numero) => {
+    return `Número: ${numero}`;
+})
+console.log(resultado);
+*/
+
+//Find
+/*const item = array.find((element) => {
+    return condicao;
+});
+*/
+
+/*const people = [
+    {name: "Lucas", age: 35},
+    {name: "Maria", age: 17},
+    {name: "João", age: 18},
+    {name: "Ana", age: 22},
+];*/
+
+/*const maria = people.find((person) => {
+    return person.name === "Maria";
+});
+console.log(maria);
+*/
+
+//Encontrar número
+/*const numeros = [5,10,15,20,25];
+const encontrado = numeros.find((numero) => {
+    return numero > 18;
+});
+console.log(encontrado);
+*/
+
+/*const usuarios = [
+    {id:1, nome:"Lucas"},
+    {id:2, nome:"Maria"},
+    {id:3, nome:"João"}
+];
+const usuario = usuarios.find((u) => {
+    return u.id === 2;
+});
+console.log(usuario);
+*/
+
+//Encontre o primeiro número negativo do array.
+/*const numeros = [5,8,-2,10,-7];
+const negativo = numeros.find((numero) => {
+    return numero < 0;
+});
+*/
+
+//Reduce
+/*Array.reduce((acc, element) => {
+    return novoACC;
+}, valorInicial);
+*/
+
+/*const numeros = [10 , 20, 30];
+
+const soma = numeros.reduce((acc, numero) => {
+    console.log("ACC:", acc);
+    console.log("Número atual:", numero);
+    return acc + numero;
+}, 0);
+console.log(soma);
+*/
+
+/*const people = [
+    {name: "Lucas", age: 35},
+    {name: "Maria", age: 17},
+    {name: "João", age: 18},
+    {name: "Ana", age: 22},
+];*/
+/*const totalIdade = people.reduce((acc, person) => {
+    return acc + person.age;
+}, 0);
+console.log(totalIdade)
+*/
+
+/*const nomes = people.reduce((acc, person) => {
+    return acc + " " + person.name;
+}, "");
+console.log(nomes);
+*/
+
+//Multiplicar números
+/*const numeros = [2,3,4];
+const Multiplicar = numeros.reduce((acc, numero) => {
+    return acc * numero;
+}, 1);
+console.log(Multiplicar)
+*/
+
+//Somar preços de produtos
+/*const carrinho = [
+    {produto: "Mouse", preco:50}
+    {produto: "Teclado", preco:100}
+    {produto: "Monitor", preco:900}
+];
+const total = carrinho.reduce((acc, item) => {
+    return acc + item.preco;
+}, 0);
+console.log(total);
+*/
+
+//Contar quantidade de palavras de letras
+/*const palavras = ["oi","javascript","html"];
+
+const totalLetras = palavras.reduce((acc, palavra) => {
+    return acc + palavra.length;
+}, 0);
+console.log(totalLetras);
+*/
